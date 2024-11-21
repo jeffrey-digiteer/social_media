736f1544d734332dd261cbddeb193c28a6595f19
Raphael Padua <raph@digiteer.digital>
2024-11-21 11:48:52 +0800

    added scoping for users devise

--- config/routes/site.rb
+++ config/routes/site.rb
@@ -1,3 +1,15 @@
+devise_for :users,
+  controllers: {
+    sessions: "site/sessions",
+    registrations: "site/registrations"
+  },
+  path: "/",
+  path_names: {
+    sign_in: "login",
+    sign_out: "logout",
+    sign_up: "register"
+  }
+
 namespace :site, path: "/" do
   resources :posts
 end
