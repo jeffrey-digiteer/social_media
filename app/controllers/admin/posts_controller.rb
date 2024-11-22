3ba04810c446e356b00d3a4727034a82ee68db95
jeffrey-digiteer <jeffrey@digiteer.digital>
2024-11-22 18:16:10 +0800

    added scoping and feat admin set featured/hide/show posts

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,4 +1,7 @@
 class Admin::PostsController < AdminController
+  include Devise::Controllers::Helpers
+  before_action :set_post, only: %i[ show edit update destroy ]
+
   # GET /posts or /posts.json
   def index
     @posts = Post.all
@@ -7,5 +10,57 @@ class Admin::PostsController < AdminController
   # GET /posts/1 or /posts/1.json
   def show
   end
+
+  # GET /posts/new
+  def new
+    @post = Post.new
+  end
+
+  # GET /posts/1/edit
+  def edit
+  end
+
+  # POST /posts or /posts.json
+  def create
+    @post = Post.new(post_params)
+
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to [ :admin, @post ], notice: "Post was successfully created." }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /posts/1 or /posts/1.json
+  def update
+    respond_to do |format|
+      if @post.update(post_params)
+        format.html { redirect_to [ :admin, @post ], notice: "Post was successfully updated." }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /posts/1 or /posts/1.json
+  def destroy
+    @post.destroy!
+
+    respond_to do |format|
+      format.html { redirect_to admin_posts_path, status: :see_other, notice: "Post was successfully destroyed." }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_post
+      @post = Post.find(params.expect(:id))
+    end
+
+    # Only allow a list of trusted parameters through.
+    def post_params
+      params.expect(post: [ :name, :content, :publish_date, :featured, :active, :user_id ])
+    end
 end
-  
